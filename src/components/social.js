b6000b5c2586a3dd3338df67423a05bcd2a88722
Robert A <daspanisharmada@gmail.com>
2019-10-10 01:50:22 -0700

    social

---
+++ src/components/social.js
@@ -0,0 +1,89 @@
+import React from 'react'
+import styled from 'styled-components'
+
+import Github from '../images/github.svg'
+import Linkedin from '../images/linkedin.svg'
+import Twitter from '../images/twitter.svg'
+import LargeDot from '../images/large-blue-dot.svg'
+import Behance from '../images/behance.svg'
+
+const SocialBar = styled.section`
+  position: fixed;
+  bottom: 0px;
+  left: 40px;
+  height: fit-content;
+  z-index: 2;
+  display: flex;
+  justify-content: center;
+  align-items: flex-end;
+  padding-bottom: calc(1.618rem * 3);
+`
+
+const SocialLinks = styled.ul`
+  display: flex;
+  flex-direction: column;
+  list-style-type: none;
+`
+
+const SocialLink = styled.li`
+  padding-top: 1rem;
+`
+
+const GithubImage = styled.img`
+  height: 32px;
+  width: 32px;
+  margin: 0;
+  padding: 0;
+`
+
+const TwitterImage = styled.img`
+  height: 32px;
+  width: 32px;
+  margin: 0;
+  padding: 0;
+`
+
+const BehanceImage = styled.img`
+  height: 32px;
+  width: 32px;
+  margin: 0;
+  padding: 0;
+`
+
+const LinkedinImage = styled.img`
+  height: 32px;
+  width: 32px;
+  margin: 0;
+  padding: 0;
+`
+
+const Social = () => {
+  return (
+    <SocialBar>
+      <SocialLinks>
+        <SocialLink>
+          <a href="https://github.com/AwakenedMind">
+            <GithubImage src={Github} />
+          </a>
+        </SocialLink>
+        <SocialLink>
+          <a href="https://twitter.com/RobArteaga4">
+            <TwitterImage src={Twitter} />
+          </a>
+        </SocialLink>
+        <SocialLink>
+          <a href="https://www.linkedin.com/in/robert-arteaga-a3ab89127/">
+            <LinkedinImage src={Linkedin} />
+          </a>
+        </SocialLink>
+        <SocialLink>
+          <a href="https://www.behance.net/roba1">
+            <BehanceImage src={Behance} />
+          </a>
+        </SocialLink>
+      </SocialLinks>
+    </SocialBar>
+  )
+}
+
+export default Social
